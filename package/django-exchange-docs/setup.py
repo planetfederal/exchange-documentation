b00f436f0bb6cf77ecadae08ac173f949b2ff279
Daniel Berry <dberry@boundlessgeo.com>
2016-10-03 20:54:42 -0500

    updating package patch version

--- package/django-exchange-docs/setup.py
+++ package/django-exchange-docs/setup.py
@@ -6,7 +6,7 @@ with open('README.rst', 'r') as inp:
 
 setup(
     name='django-exchange-docs',
-    version='1.1.0',
+    version='1.0.1',
     author='Boundless Spatial',
     author_email='contact@boundlessgeo.com',
     url='https://github.com/boundlessgeo/exchange-documentation',
