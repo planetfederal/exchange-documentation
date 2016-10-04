fe1bf13bea577c08b1dbed5baf059dca39d36485
Daniel Berry <dberry@boundlessgeo.com>
2016-10-03 20:29:02 -0500

    Bump version: 1.1.0 → 1.1.0

--- package/django-exchange-docs/setup.py
+++ package/django-exchange-docs/setup.py
@@ -1,16 +1,16 @@
 import os
 from setuptools import setup, find_packages
 
-with open('README.md', 'r') as inp:
+with open('README.rst', 'r') as inp:
     LONG_DESCRIPTION = inp.read()
 
 setup(
     name='django-exchange-docs',
-    version='1.1.0a1',
+    version='1.1.0',
     author='Boundless Spatial',
     author_email='contact@boundlessgeo.com',
-    url='https://github.com/boundlessgeo/connect-learning',
-    download_url="https://github.com/boundlessgeo/connect-learning",
+    url='https://github.com/boundlessgeo/exchange-documentation',
+    download_url="https://github.com/boundlessgeo/exchange-documentation",
     description="A Django wrapper for Boundless Exchange Documentation.",
     long_description=LONG_DESCRIPTION,
     license='GPLv2',
